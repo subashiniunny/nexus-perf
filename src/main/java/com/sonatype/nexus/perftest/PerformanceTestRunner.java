24cc6ffd5de738f0019c9bb87a0039eef79f49bc
Tamas Cservenak <tamas@cservenak.net>
2016-04-27 17:59:36 +0200

    Reshuffle

--- src/main/java/com/sonatype/nexus/perftest/PerformanceTestRunner.java
+++ src/main/java/com/sonatype/nexus/perftest/PerformanceTestRunner.java
@@ -7,6 +7,9 @@
 package com.sonatype.nexus.perftest;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.Properties;
 
 import com.fasterxml.jackson.databind.BeanProperty;
 import com.fasterxml.jackson.databind.DeserializationContext;
@@ -14,23 +17,45 @@ import com.fasterxml.jackson.databind.InjectableValues;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 
-public class PerformanceTestRunner {
+public class PerformanceTestRunner
+{
   public static void main(String[] args) throws Exception {
+    File perfProperties = new File("perf.properties");
+    if (!perfProperties.isFile()) {
+      System.out.printf("No data?");
+      System.exit(1);
+    }
+
+    Properties perf = new Properties();
+    try (InputStream is = new FileInputStream(perfProperties)) {
+      perf.load(is);
+    }
+    String scenario = perf.getProperty("perftest.scenario");
+    File scenarioFile = new File(scenario).getCanonicalFile();
+    if (!scenarioFile.isFile()) {
+      System.out.printf("Scenario " + scenario + " found at " + scenarioFile);
+      System.exit(1);
+    }
+    new PerformanceTestRunner().run(scenarioFile);
+  }
+
+  private void run(final File dataDirectory) throws Exception {
     final Nexus nexus = new Nexus();
     ObjectMapper mapper = new XmlMapper();
-    mapper.setInjectableValues(new InjectableValues() {
+    mapper.setInjectableValues(new InjectableValues()
+    {
       @Override
       public Object findInjectableValue(Object valueId, DeserializationContext ctxt, BeanProperty forProperty,
-          Object beanInstance) {
+                                        Object beanInstance)
+      {
         if (Nexus.class.getName().equals(valueId)) {
           return nexus;
         }
         return null;
       }
     });
-    File src = new File(args[0]).getCanonicalFile();
-    System.out.format("Using test configuration %s\n", src);
-    PerformanceTest test = mapper.readValue(src, PerformanceTest.class);
+    System.out.format("Using test configuration %s\n", dataDirectory);
+    PerformanceTest test = mapper.readValue(dataDirectory, PerformanceTest.class);
     test.run();
     System.out.println("Exit");
     System.exit(0);
