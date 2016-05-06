93dc182063c95ed303c0819f4c195341559ba590
Tamas Cservenak <tamas@cservenak.net>
2016-05-06 13:04:43 +0200

    Add proxy pumper
    This implementation is only to produce 404s remotely but keep
    proxy and NFC very busy

---
+++ src/main/java/com/sonatype/nexus/perftest/maven/GeneratedDownloadPaths.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2007-2013 Sonatype, Inc. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the terms of the Eclipse Public License Version 1.0,
+ * which accompanies this distribution and is available at http://www.eclipse.org/legal/epl-v10.html.
+ */
+package com.sonatype.nexus.perftest.maven;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+/**
+ * Generated download paths that most probably does not exists on remote. Useful to exercise HttpClient and proxy
+ * capabilities of Nexus.
+ */
+public class GeneratedDownloadPaths
+    implements DownloadPaths
+{
+  private final AtomicInteger nextIndex = new AtomicInteger(0);
+
+  private final String prefix;
+
+  @JsonCreator
+  public GeneratedDownloadPaths(@JsonProperty(value = "prefix") String prefix)
+  {
+    this.prefix = prefix;
+  }
+
+  @Override
+  public String getNext() {
+    return prefix + "/" + System.nanoTime();
+  }
+
+  @Override
+  public Iterable<String> getAll() {
+    throw new RuntimeException("getAll() or " + getClass().getSimpleName() + " cannot be invoked");
+  }
+}
