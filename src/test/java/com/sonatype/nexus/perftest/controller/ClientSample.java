eeb4621c132a8e5bad4c4c98de30b1d5f6f5a5dc
Alin Dreghiciu <adreghiciu@gmail.com>
2016-06-29 13:02:15 +0300

    Add more example methods

--- src/test/java/com/sonatype/nexus/perftest/controller/ClientSample.java
+++ src/test/java/com/sonatype/nexus/perftest/controller/ClientSample.java
@@ -6,8 +6,11 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+import javax.management.remote.JMXServiceURL;
+
 import org.junit.Test;
 
+import static java.util.stream.Collectors.toList;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
@@ -15,49 +18,19 @@ import static org.hamcrest.Matchers.hasSize;
 
 public class ClientSample
 {
+  private static final Collection<JMXServiceURL> URLS = JMXServiceURLs.of(
+      "192.168.1.6:5001",
+      "192.168.1.3:5001",
+      //"localhost:5001",
+      //"localhost:5002",
+      "localhost:5003"
+  );
+
   private static final String DATA = "all/target/all-1.0.4-SNAPSHOT-data/";
 
   @Test
-  public void sample() throws Exception {
-    AgentPool pool = new AgentPool(JMXServiceURLs.of(
-        /*"192.168.1.6:5001",
-        "192.168.1.7:5001",*/
-        "localhost:5001",
-        "localhost:5002",
-        "localhost:5003"
-    ));
-
-    //Nexus nexus = new Nexus(jmxServiceURL("localhost:1099"));
-    //nexus.addTrigger(new QueuedThreadPoolUnhealthy(
-    //    (message) -> {
-    //      System.out.println();
-    //      System.out.println(
-    //          "!!!!!!!!!!!!!!!!! Nexus is dead (" + message + ")"
-    //      );
-    //      System.out.println();
-    //      pool.releaseAll();
-    //    }
-    //));
-    //nexus.addTrigger(new GaugeTrigger<>(
-    //        Nexus.QueuedThreadPool.activeThreads,
-    //        (state, activeThreads) -> {
-    //          if (State.HIGH == state) {
-    //            System.out.println();
-    //            System.out.println(
-    //                "!!!!!!!!!!!!!!!!! Nexus is dead (" + activeThreads + ")"
-    //            );
-    //            System.out.println();
-    //            //pool.releaseAll();
-    //          }
-    //          else {
-    //            System.out.println();
-    //            System.out.println(
-    //                "!!!!!!!!!!!!!!!!! Nexus is back to life (" + activeThreads + ")"
-    //            );
-    //            System.out.println();
-    //          }
-    //        }).setHighThreshold(200)
-    //);
+  public void multipleScenarios() throws Exception {
+    AgentPool pool = new AgentPool(URLS);
 
     try {
       Collection<Agent> m01Agents = pool.acquire(2);
@@ -75,15 +48,8 @@ public class ClientSample
       m01Agents.parallelStream().forEach(Agent::start);
       m02Agents.parallelStream().forEach(Agent::start);
 
-      List<Swarm> m01Swarms = m01Agents.stream().map(Agent::getSwarms).flatMap(Collection::stream)
-          .collect(Collectors.toList());
-      m01Swarms.parallelStream().map(Swarm::getControl).forEach(control -> {
-        control.setRateMultiplier(5);
-        control.setRateSleepMillis(7);
-      });
-
-      List<Swarm> m02Swarms = m02Agents.stream().map(Agent::getSwarms).flatMap(Collection::stream)
-          .collect(Collectors.toList());
+      List<Swarm> m01Swarms = m01Agents.stream().map(Agent::getSwarms).flatMap(Collection::stream).collect(toList());
+      List<Swarm> m02Swarms = m02Agents.stream().map(Agent::getSwarms).flatMap(Collection::stream).collect(toList());
 
       m01Agents.parallelStream().forEach(Agent::waitToFinish);
       m02Agents.parallelStream().forEach(Agent::waitToFinish);
@@ -94,7 +60,51 @@ public class ClientSample
 
       m01Swarms.stream().forEach(swarm -> assertThat(swarm.get(Swarm.Failure.count), is(equalTo(0L))));
       m02Swarms.stream().forEach(swarm -> assertThat(swarm.get(Swarm.Failure.count), is(equalTo(0L))));
-      //assertThat(nexus.get(Nexus.Requests.count), is(greaterThan(500L)));
+    }
+    finally {
+      pool.releaseAll();
+    }
+  }
+
+  @Test
+  public void npm01() throws Exception {
+    AgentPool pool = new AgentPool(URLS);
+
+    try {
+      Collection<Agent> agents = pool.acquireAll();
+
+      Map<String, String> overrides = new HashMap<>();
+      overrides.put("nexus.baseurl", "http://192.168.1.99:8081/nexus");
+      overrides.put("test.duration", "5 MINUTES");
+      //overrides.put("test.duration", "20 SECONDS");
+
+      agents.parallelStream().forEach(agent -> agent.load(DATA + "npm01-1.0.4-SNAPSHOT", overrides));
+      agents.parallelStream().forEach(Agent::start);
+
+      List<Swarm> swarms = agents.stream().map(Agent::getSwarms).flatMap(Collection::stream).collect(toList());
+      swarms.parallelStream().map(Swarm::getControl).forEach(control -> {
+        control.setRateSleepMillis(1);
+        control.setRateSleepMillis(10);
+      });
+
+      agents.parallelStream().forEach(Agent::waitToFinish);
+      swarms.parallelStream().map(Swarm::getControl).forEach(control -> {
+        control.getFailures().stream().forEach(failure -> System.out.println("-------- " + failure));
+      });
+
+      swarms.stream().forEach(swarm -> assertThat(swarm.get(Swarm.Failure.count), is(equalTo(0L))));
+    }
+    finally {
+      pool.releaseAll();
+    }
+  }
+
+  @Test
+  public void stopAll() throws Exception {
+    AgentPool pool = new AgentPool(URLS);
+
+    try {
+      pool.acquireAll();
     }
     finally {
       pool.releaseAll();
